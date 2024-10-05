4a0f28f6f8a4cd9d8e4433f4725f2b4ffa6a02f9
Aaron Murray <plugins@omv-extras.org>
2024-10-05 13:08:08 -0500

    save state of show environment and show override checkboxes in File editor
    Signed-off-by: Aaron Murray <plugins@omv-extras.org>

--- usr/share/openmediavault/confdb/migrations.d/conf.service.compose_7.2.8.sh
+++ usr/share/openmediavault/confdb/migrations.d/conf.service.compose_7.2.8.sh
@@ -8,5 +8,19 @@ if ! omv_config_exists "/config/services/compose/showcmd"; then
   omv_config_add_key "/config/services/compose" "showcmd" "0"
 fi
 
+xpath="/config/services/compose/files/file"
+count=$(omv_config_get_count "${xpath}");
+index=1;
+while [ ${index} -le ${count} ]; do
+  pos="${xpath}[position()=${index}]"
+  if ! omv_config_exists "${pos}/showenv"; then
+    omv_config_add_key "${pos}" "showenv" "0"
+  fi
+  if ! omv_config_exists "${pos}/showoverride"; then
+    omv_config_add_key "${pos}" "showoverride" "0"
+  fi
+  index=$(( index + 1 ))
+done;
+
 exit 0
 
