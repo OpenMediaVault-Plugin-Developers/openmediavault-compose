4db87a246d5d63c1f31985ab635a575d32b3caa1
Aaron Murray <plugins@omv-extras.org>
2024-12-23 17:18:21 -0600

    add excludes to backup jobs
    Signed-off-by: Aaron Murray <plugins@omv-extras.org>

--- usr/share/openmediavault/confdb/migrations.d/conf.service.compose_7.3.sh
+++ usr/share/openmediavault/confdb/migrations.d/conf.service.compose_7.3.sh
@@ -8,5 +8,16 @@ if ! omv_config_exists "/config/services/compose/configs"; then
   omv_config_add_node "/config/services/compose" "configs"
 fi
 
+xpath="/config/services/compose/jobs/job"
+count=$(omv_config_get_count "${xpath}");
+index=1;
+while [ ${index} -le ${count} ]; do
+  pos="${xpath}[position()=${index}]"
+  if ! omv_config_exists "${pos}/excludes"; then
+    omv_config_add_key "${pos}" "excludes" ""
+  fi
+  index=$(( index + 1 ))
+done;
+
 exit 0
 
