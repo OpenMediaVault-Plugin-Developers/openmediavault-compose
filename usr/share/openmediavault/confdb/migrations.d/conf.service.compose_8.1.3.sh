8366d2b55cf80b468d6e32bfe468041e7a826c2f
Aaron Murray <plugins@omv-extras.org>
2026-02-10 23:31:30 +0000

    add borg keep setting
    Signed-off-by: Aaron Murray <plugins@omv-extras.org>

--- usr/share/openmediavault/confdb/migrations.d/conf.service.compose_8.1.3.sh
+++ usr/share/openmediavault/confdb/migrations.d/conf.service.compose_8.1.3.sh
@@ -9,5 +9,8 @@ echo "Updating database ..."
 if ! omv_config_exists "/config/services/compose/backupbackend"; then
   omv_config_add_key "/config/services/compose" "backupbackend" "rsync"
 fi
+if ! omv_config_exists "/config/services/compose/borgkeep"; then
+  omv_config_add_key "/config/services/compose" "borgkeep" "7"
+fi
 
 exit 0
