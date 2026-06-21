78ce2e0c04c29dcb13144b899dc313199199c83b
Aaron Murray <plugins@omv-extras.org>
2026-06-21 16:08:12 +0000

    add migration script
    Signed-off-by: Aaron Murray <plugins@omv-extras.org>

---
+++ usr/share/openmediavault/confdb/migrations.d/conf.service.compose_8.1.22.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+. /usr/share/openmediavault/scripts/helper-functions
+
+echo "Updating database ..."
+
+xpath="/config/services/compose/jobs/job"
+
+xmlstarlet sel -t -m "${xpath}" -v "uuid" -n ${OMV_CONFIG_FILE} |
+  xmlstarlet unesc |
+  while read uuid; do
+    if ! omv_config_exists "${xpath}[uuid='${uuid}']/skipstartstop"; then
+      omv_config_add_key "${xpath}[uuid='${uuid}']" "skipstartstop" "0"
+    fi
+  done
+
+omv_module_set_dirty compose
+
+exit 0
